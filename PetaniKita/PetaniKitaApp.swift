bd78b01d283196b67f8359e263b102daf5be0228
Risang Baskoro <risangbaskoro@gmail.com>
2023-06-21 16:01:06 +0700

    Initial Commit

---
+++ PetaniKita/PetaniKitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetaniKitaApp.swift
+//  PetaniKita
+//
+//  Created by Risang Baskoro on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PetaniKitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
