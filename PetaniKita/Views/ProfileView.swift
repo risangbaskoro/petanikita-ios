b263e938e2f073f99d2f4d621a1e03bdf3833dbe
Risang Baskoro <risangbaskoro@gmail.com>
2023-06-23 04:45:34 +0700

    Remove ProfileViewViewModel

--- PetaniKita/Views/ProfileView.swift
+++ PetaniKita/Views/ProfileView.swift
@@ -8,32 +8,24 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @StateObject var viewModel = ProfileViewViewModel()
-    
-    
     var body: some View {
         NavigationView {
             VStack {
                 VStack {
-                    if viewModel.user != nil {
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                            .padding()
-                            .background(.gray)
-                            .clipShape(Circle())
-
-                        Text(viewModel.user.name)
-                            .font(.title)
-                        Text(viewModel.user.email)
-                        Text(viewModel.user.phone)
-                    } else {
-                        ProgressView()
-                            .progressViewStyle(.circular)
-                    }
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100)
+                        .padding()
+                        .background(.gray)
+                        .clipShape(Circle())
+                    
+                    Text("John")
+                        .font(.title)
+                    Text("Email")
+                    Text("Phone")
                 }
-
+                
                 Spacer()
                     .navigationTitle("Profile")
                     .onAppear {
