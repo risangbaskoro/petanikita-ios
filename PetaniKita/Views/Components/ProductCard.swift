5f3927544d2a316548d60b9b80156440c1992b27
Risang Baskoro <risangbaskoro@gmail.com>
2023-06-24 21:30:40 +0700

    fix product card image placeholder

--- PetaniKita/Views/Components/ProductCard.swift
+++ PetaniKita/Views/Components/ProductCard.swift
@@ -12,25 +12,28 @@ struct ProductCard: View {
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            CacheAsyncImage(url: URL(string: product.images[0])!) { phase in
-                switch phase {
-                case .empty:
-                    ZStack {
-                        Image(systemName: "photo.fill")
-                            .padding(2)
-                        ProgressView()
-                            .progressViewStyle(.circular)
+            if !product.images.isEmpty {
+                CacheAsyncImage(url: URL(string: product.images[0])!) { phase in
+                    switch phase {
+                    case .empty:
+                        ZStack {
+                            ProductCardImagePlaceholder()
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                        }
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .frame(maxWidth: .infinity)
+                            .scaledToFit()
+                    case .failure(_):
+                        ProductCardImagePlaceholder()
+                    @unknown default:
+                        fatalError()
                     }
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFill()
-                case .failure(_):
-                    Image(systemName: "photo.fill")
-                        .padding(2)
-                @unknown default:
-                    fatalError()
                 }
+            } else {
+                ProductCardImagePlaceholder()
             }
             
             HStack(alignment: .center) {
@@ -52,8 +55,20 @@ struct ProductCard: View {
     }
 }
 
+struct ProductCardImagePlaceholder: View {
+    var body: some View {
+        Image(systemName: "photo.fill")
+            .resizable()
+            .scaledToFit()
+            .padding(.bottom, 60)
+            .padding(.horizontal)
+            .foregroundStyle(Color.gray)
+            .frame(maxWidth: 200, maxHeight: 350)
+    }
+}
+
 struct ProductCard_Previews: PreviewProvider {
     static var previews: some View{
-        ProductCard(product: Product(id: 1, name: "Test", description: "Mantap", price: 3400, stock: 20, images: ["https://images.unsplash.com/photo-1585664428450-1665a336eb19?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=256&ixid=MnwxfDB8MXxyYW5kb218MHx8cGxhbnR8fHx8fHwxNjg3NDI0MTk5&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=256"]))
+        ProductCard(product: Product(id: 1, name: "Test", description: "Mantap", price: 3400, stock: 20, images: []))
     }
 }
